8c31b70018e2f1c3649128600bb44608556c07f6
After Dark Codes <after.dark.codes@gmail.com>
2022-04-06 10:18:54 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Header from "./header";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Rem Jobs</title>
+      </Head>
+      <Header />
+    </>
+  );
+}
