b3aac5346530e750739f0799bcbb83ea16641ec6
After Dark Codes <after.dark.codes@gmail.com>
2022-04-12 05:36:33 -0700

    Update JobList

--- components/JobList.js
+++ components/JobList.js
@@ -1,20 +1,34 @@
 import JobItem from "./JobItem";
 import { useRecoilState } from "recoil";
 import { jobsState } from "../atoms/jobsAtom";
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 function JobList() {
   const [filter, setFilter] = useRecoilState(jobsState);
+
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    return onSnapshot(
+      query(collection(db, "jobs"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setJobs(snapshot.docs);
+      }
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [db]);
   return (
     <div className="">
       <pre>{filter}</pre>
-
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
+      {jobs.map((job) => (
+        <JobItem
+          key={job.id}
+          id={job.id}
+          position={job.data().position}
+          category={job.data().category}
+        />
+      ))}
       <JobItem />
     </div>
   );
